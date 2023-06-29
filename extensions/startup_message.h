60de0b5d929e883ec18f38c9b6d234b7682d3b87
Samdal <samdal@protonmail.com>
2023-06-29 20:11:18 +0200

    add my config personal config because it's WIP

--- extensions/startup_message.h
+++ extensions/startup_message.h
@@ -3,7 +3,6 @@
 
 static const char* const welcome[] = {"Welcome to se!", "Good day, Human", "Happy Coding", "se: the Simple Editor",
     "Time to get some progress done!", "Ready for combat", "Initialising...Done",  "loaded in %%d seconds",
-    "Fun fact: vscode has over two times as many lines describing dependencies than se has in total",
     "You look based", "Another day, another bug to fix", "Who needs a mouse ¯\\_(ツ)_/¯", "grrgrrggghhaaaaaa (╯°□ °）╯︵ ┻━┻",
     "┬┴┬┤(･_├┬┴┬┴┬┴┬┤ʖ ͡°) ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴", "ʰᵉˡˡᵒ"};
 
